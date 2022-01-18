c9785ec85319ce1cba8d24faa5744c43ddbdd6b9
Thiago Teixeira <62726137+xthiagoteixeira@users.noreply.github.com>
2022-01-18 09:43:52 -0300

    POO - Aula de Orientacao a Objetos

---
+++ java-amdocs/poo/src/poo/model/Cliente.java
@@ -0,0 +1,27 @@
+package poo.model;
+
+public class Cliente {
+
+	//Atributos
+	public Integer codigo;
+	public String nome;
+	public String cpf;
+		
+	public String endereco1;
+	public String numero1;
+	public String complemento1;
+	public String cidade1;
+	public String bairro1;
+	public String estado1;
+	public String cep1;
+	
+
+	public String endereco2;
+	public String numero2;
+	public String complemento2;
+	public String cidade2;
+	public String bairro2;
+	public String estado2;
+	public String cep2;
+	
+}
---
+++ java-amdocs/segundo-programa/src/com/dio/MyFirstProgram.java
@@ -0,0 +1,14 @@
+package com.dio;
+
+import com.dio.base.Order;
+
+public class MyFirstProgram {
+
+    public static void main(String[] args) {
+
+        Order order = new Order ("code1234", 2975671681510998);
+        System.out.println(order);
+
+    }
+
+}
