15c7eba4898f1ad7020823843fc1c013eedb2a58
Thiago Teixeira <62726137+xthiagoteixeira@users.noreply.github.com>
2022-01-18 09:49:54 -0300

    Arquivos do Bootcamp Amdocs

--- java-amdocs/poo/src/poo/model/Cliente.java
+++ java-amdocs/poo/src/poo/model/Cliente.java
@@ -7,21 +7,11 @@ public class Cliente {
 	public String nome;
 	public String cpf;
 		
-	public String endereco1;
-	public String numero1;
-	public String complemento1;
-	public String cidade1;
-	public String bairro1;
-	public String estado1;
-	public String cep1;
-	
 
-	public String endereco2;
-	public String numero2;
-	public String complemento2;
-	public String cidade2;
-	public String bairro2;
-	public String estado2;
-	public String cep2;
+	public Endereco endereco;
+	public Endereco enderecoEntrega;
+	public Endereco enderecoTrabalho;
+	
+		
 	
 }
---
+++ java-amdocs/poo/src/poo/model/Endereco.java
@@ -0,0 +1,13 @@
+package poo.model;
+
+public class Endereco {
+
+	public String endereco;
+	public String numero;
+	public String complemento;
+	public String cidade;
+	public String bairro;
+	public String estado;
+	public String cep;
+	
+}
