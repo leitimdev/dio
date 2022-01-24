5982667ead727da2ec8fb3154d79ba2d75e13fe4
Thiago Teixeira <62726137+xthiagoteixeira@users.noreply.github.com>
2022-01-24 09:47:33 -0300

    Arquivos Curso da Amdocs

---
+++ java-amdocs/springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/SpringbootconfigApplication.java
@@ -0,0 +1,15 @@
+package com.digitalinnovationone.springbootconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootconfigApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringbootconfigApplication.class, args);
+
+	}
+
+}
---
+++ java-amdocs/springbootconfig/src/main/java/com/digitalinnovationone/springbootconfig/config/DBConfiguration.java
@@ -0,0 +1,41 @@
+package com.digitalinnovationone.springbootconfig.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@ConfigurationProperties("spring.datasource")
+public class DBConfiguration {
+
+    private String driverClassName;
+    private String url;
+    private String username;
+    private String password;
+
+    @Profile("dev")
+    @Bean
+    public String testeDatabaseConnection() {
+
+        System.out.println("DB connection for DEV - H2");
+        System.out.println(driverClassName);
+        System.out.println(url);
+
+        return "DB Connection to H2_TEST - Test instance";
+    }
+
+    @Profile("prod")
+    @Bean
+    public String prodDatabaseConnection() {
+
+        System.out.println("DB Connection for Production - MySQL");
+        System.out.println(driverClassName);
+        System.out.println(url);
+
+        return "DB Connection to MYSQL_PROD - Production instance";
+
+    }
+
+
+}
