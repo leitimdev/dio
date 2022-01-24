d924ac0183e29eee65386c2db7dfb54992140370
Thiago Teixeira <62726137+xthiagoteixeira@users.noreply.github.com>
2022-01-24 10:21:13 -0300

    Arquivos Amdocs

---
+++ java-amdocs/utilizandoBeans/src/main/java/com/springbeans/App.java
@@ -0,0 +1,26 @@
+package com.springbeans;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+
+public class App {
+	
+	public static void main(String[] args) {
+		
+		ApplicationContext factory = new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		Livro livro = factory.getBean(Livro.class);
+		livro.setCodigo("D32323");
+		livro.setNome("Harry Potter");
+		
+		Autor autor = factory.getBean(Autor.class);
+		autor.setNome("J.K. Rowling");
+		
+		
+		livro.exibir();
+		
+		((AbstractApplicationContext) factory).close();
+	}
+
+}
