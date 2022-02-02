fe481a365d508632051b3dfb3ee65e11d92a0cc6
Thiago Teixeira <62726137+xthiagoteixeira@users.noreply.github.com>
2022-02-02 09:55:58 -0300

    Arquivos Java Sportheca

---
+++ java-kotlin-sportheca/projetono/src/dio/projetono/Main.java
@@ -0,0 +1,24 @@
+package dio.projetono;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        No no1 = new No("Conteudo no1");
+        No no2 = new No("Conteudo no2");
+
+        no1.setProximoNo(no2);
+
+        No no3 = new No("Conteudo no3");
+        no2.setProximoNo(no3);
+
+        No no4 = new No("Conteudo no4");
+        no3.setProximoNo(no4);
+
+
+
+
+
+    }
+
+}
