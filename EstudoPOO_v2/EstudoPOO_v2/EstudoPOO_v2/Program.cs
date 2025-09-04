339a3de8761135188c048c897d91c6b01e690c12
Thiago <thiagoteixeira@uol.com.br>
2025-09-03 22:48:26 -0300

    Continuação do Estudo de POO na Avanade

---
+++ EstudoPOO_v2/EstudoPOO_v2/EstudoPOO_v2/Program.cs
@@ -0,0 +1,16 @@
+﻿using EstudoPOO_v2.Models;
+using System;
+
+ContaCorrente conta = new ContaCorrente(123, 456789, 1000);
+conta.Depositar(500);
+conta.Sacar(200);
+conta.ExibirSaldo();
+
+
+
+
+/* Pessoa pessoa1 = new Pessoa();
+pessoa1.Nome = "João";
+pessoa1.Idade = 30;
+pessoa1.Apresentar();
+ */
