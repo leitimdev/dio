8ade127c1811beaffd046e3ac1e1f4f718a93f5c
Thiago <thiagoteixeira@uol.com.br>
2025-09-17 14:44:39 -0300

    Agenda Swagger+MySQL na Avanade

---
+++ ModuloAPI/Program.cs
@@ -0,0 +1,27 @@
+﻿using ModuloAPI.Context;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AgendaContext>(options =>
+    options.UseMySql(builder.Configuration.GetConnectionString("ConexaoPadrao"), new MySqlServerVersion(new Version(8, 0, 32))));
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
